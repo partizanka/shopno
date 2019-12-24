6b6035953a8ef58a845cb6cd783aa3281c6934f5
Anastasia Khmara <zavetnaya.13@gmail.com>
2019-12-24 20:20:13 +0300

    change mobile adaptive

--- js/mine.js
+++ js/mine.js
@@ -3,25 +3,10 @@ $(document).ready(function() {
     items: 1,
     center: true
   });
-});
 
-$(document).ready(function() {
   $("#owl-testimonial").owlCarousel({
     items: 1,
     nav: true,
     center: true
-  });
-
-  var owl = $('#owl-testimonial');
-  owl.owlCarousel();
-  // Go to the next item
-  $('.owl-next').click(function() {
-    owl.trigger('next.owl.carousel');
-  });
-  // Go to the previous item
-  $('.owl-prev').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [300]);
   });
 });
